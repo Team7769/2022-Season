69a03ada34a6db89a1fb6bfb54489639311f152a
Colin Ross <cross1@ltu.edu>
2022-01-15 13:57:23 -0500

    Merge Pull Request #1: Added comment for robot periodic method. (#1)
    Co-authored-by: Colin Ross <crosstcp@gmail.com>

--- 2022 Season/src/main/java/frc/robot/Robot.java
+++ 2022 Season/src/main/java/frc/robot/Robot.java
@@ -39,7 +39,9 @@ public class Robot extends TimedRobot {
    * SmartDashboard integrated updating.
    */
   @Override
-  public void robotPeriodic() {}
+  public void robotPeriodic() {
+    // This is the robot periodic method.
+  }
 
   /**
    * This autonomous (along with the chooser code above) shows how to select between different
