4b5aa3a2206b704e90b8127327c22ccdca77915a
Colin Ross <crosstcp@gmail.com>
2022-01-21 18:45:22 -0500

    Added differential drive and inverted the right side motors.

--- 2022 Season/src/main/java/frc/robot/Subsystems/Drivetrain.java
+++ 2022 Season/src/main/java/frc/robot/Subsystems/Drivetrain.java
@@ -3,12 +3,15 @@ package frc.robot.Subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
 public class Drivetrain {
     
     private CANSparkMax _leftFrontMotor;
     private CANSparkMax _leftRearMotor;
     private CANSparkMax _rightFrontMotor;
     private CANSparkMax _rightRearMotor;
+    private DifferentialDrive _robotDrive;
 
     private static Drivetrain _instance;
 
@@ -24,14 +27,17 @@ public class Drivetrain {
     public Drivetrain()
     {
         _leftFrontMotor = new CANSparkMax(3, MotorType.kBrushless);
-
         _leftRearMotor = new CANSparkMax(4, MotorType.kBrushless);
 
         _rightFrontMotor = new CANSparkMax(5, MotorType.kBrushless);
+        _rightFrontMotor.setInverted(true);
 
         _rightRearMotor = new CANSparkMax(6, MotorType.kBrushless);
+        _rightRearMotor.setInverted(true);
 
         _leftRearMotor.follow(_leftFrontMotor);
         _rightRearMotor.follow(_rightFrontMotor);
+
+        _robotDrive = new DifferentialDrive(_leftFrontMotor, _rightFrontMotor);
     }
 }
