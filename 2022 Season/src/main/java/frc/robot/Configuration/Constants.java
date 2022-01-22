673ef312e4664979cebab7152d0db9c2cc4f0a04
Colin Ross <crosstcp@gmail.com>
2022-01-22 15:46:00 -0500

    Added encoders and gyro to the drivetrain.

--- 2022 Season/src/main/java/frc/robot/Configuration/Constants.java
+++ 2022 Season/src/main/java/frc/robot/Configuration/Constants.java
@@ -2,11 +2,25 @@ package frc.robot.Configuration;
 
 public final class Constants {
 
+    // Usb Slots
     public static final int kDriverControllerUsbSlot = 0;
     public static final int kOperatorControllerUsbSlot = 1;
     
+    // CAN Device Ids
     public static final int kLeftFrontDriveDeviceId = 2;
     public static final int kLeftRearDriveDeviceId = 3;
     public static final int kRightFrontDriveDeviceId = 4;
     public static final int kRightRearDriveDeviceId = 5;
+    
+    //DIO
+    public static final int kLeftEncoderPortA = 0;
+    public static final int kLeftEncoderPortB = 1;
+    public static final int kRightEncoderPortA = 2;
+    public static final int kRightEncoderPortB = 3;
+
+    // Test Robot Configuration
+
+    public static final int kDriveEncoderTicksPerRevolution = 2048;
+    public static final double kDriveWheelDiameter = 0.15;
+    public static final double kDriveDistancePerPulse = (kDriveWheelDiameter * Math.PI) / kDriveEncoderTicksPerRevolution;
 }
--- 2022 Season/src/main/java/frc/robot/Subsystems/Drivetrain.java
+++ 2022 Season/src/main/java/frc/robot/Subsystems/Drivetrain.java
@@ -1,11 +1,14 @@
 package frc.robot.Subsystems;
 
+import com.kauailabs.navx.frc.AHRS;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.I2C.Port;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Configuration.Constants;
 
 public class Drivetrain implements ISubsystem {
@@ -15,6 +18,9 @@ public class Drivetrain implements ISubsystem {
     private CANSparkMax _rightFrontMotor;
     private CANSparkMax _rightRearMotor;
     private DifferentialDrive _robotDrive;
+    private Encoder _leftDriveEncoder;
+    private Encoder _rightDriveEncoder;
+    private AHRS _gyro;
 
     private static Drivetrain _instance;
 
@@ -41,6 +47,14 @@ public class Drivetrain implements ISubsystem {
         _leftRearMotor.follow(_leftFrontMotor);
         _rightRearMotor.follow(_rightFrontMotor);
 
+        _gyro = new AHRS(Port.kMXP);
+
+        _leftDriveEncoder = new Encoder(Constants.kLeftEncoderPortA, Constants.kLeftEncoderPortB);
+        _leftDriveEncoder.setDistancePerPulse(Constants.kDriveDistancePerPulse);
+
+        _rightDriveEncoder = new Encoder(Constants.kRightEncoderPortA, Constants.kRightEncoderPortB, true);
+        _rightDriveEncoder.setDistancePerPulse(Constants.kDriveDistancePerPulse);
+
         _robotDrive = new DifferentialDrive(_leftFrontMotor, _rightFrontMotor);
     }
 
@@ -50,10 +64,18 @@ public class Drivetrain implements ISubsystem {
         _leftFrontMotor.setIdleMode(IdleMode.kCoast);
         _robotDrive.arcadeDrive(throttle, turn);
     }
+    
+    public double getHeading() {
+        return Math.IEEEremainder(_gyro.getAngle(), 360) * (-1.0);
+    }
 
     public void LogTelemetry() {
         // TODO Auto-generated method stub
-        
+        SmartDashboard.putNumber("leftDriveDistance", _leftDriveEncoder.getDistance());
+        SmartDashboard.putNumber("leftDriveRate", _leftDriveEncoder.getRate());
+        SmartDashboard.putNumber("rightDriveDistance", _rightDriveEncoder.getDistance());
+        SmartDashboard.putNumber("rightDriveRate", _rightDriveEncoder.getRate());
+        SmartDashboard.putNumber("gyroHeading", getHeading());
     }
 
     public void ReadDashboardData() {
