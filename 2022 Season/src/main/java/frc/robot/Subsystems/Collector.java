2820bf901b8500ae999a4f85d5ca50bd8b7065e3
Codingwithcn <codingwithcn@gmail.com>
2022-02-12 13:43:42 -0500

    implemented collect motor

--- 2022 Season/src/main/java/frc/robot/Subsystems/Collector.java
+++ 2022 Season/src/main/java/frc/robot/Subsystems/Collector.java
@@ -1,9 +1,15 @@
 package frc.robot.Subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 public class Collector implements ISubsystem {
 
 private static Collector _instance;   
     
+    private CANSparkMax _CollectorMotor;
+
     public static Collector GetInstance()
     {
         if (_instance == null)
@@ -15,7 +21,24 @@ private static Collector _instance;
 
     public Collector() 
     { 
-        
+        _CollectorMotor = new CANSparkMax(13, MotorType.kBrushless);
+        _CollectorMotor.setInverted(true);
+        _CollectorMotor.setIdleMode(IdleMode.kBrake);
+    }
+
+    /** 
+     * Moves collect motor to a certain position 
+     * @param speed - Speed you want to set motor to
+    */
+    public void moveCollect(double speed) {
+        _CollectorMotor.set(speed);
+    }
+
+    /**
+     * Stops the collect motor from moving
+     */
+    public void stopCollect() {
+        _CollectorMotor.set(0.0);
     }
 
     public void LogTelemetry() {
